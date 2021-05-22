27f1ce9d168ca7cdc2aa2158dbd0d34ea1526a32
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-05-22 15:59:53 +0900

    Pre-rendering for post detail page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,18 +1,16 @@
 import ReactMarkdown from 'react-markdown'
 import PostHeader from './post-header'
 import classes from './post-content.module.css'
-import { DUMMY_POSTS } from '../../../pages/index'
-
-const DUMMY_POST = DUMMY_POSTS[0]
-DUMMY_POST.content = '# This is a first post'
 
 function PostContent(props) {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+  const { slug, title, content, image } = props.post
+
+  const imagePath = `/images/posts/${slug}/${image}`
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={title} image={imagePath} />
+      <ReactMarkdown>{content}</ReactMarkdown>
     </article>
   )
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,31 @@
 import PostContent from '../../components/posts/post-detail/post-content'
+import { getPostData, getPostsFiles } from '../../lib/posts-util'
 
 function PostDetailPage(props) {
   return (
-    <PostContent />
+    <PostContent post={props.post} />
   )
+}
+
+export function getStaticProps(context) {
+  const { slug } = context.params
+  const postData = getPostData(slug)
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 600
+  }
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles()
+  const fileSlugs = postFileNames.map(fileName => fileName.replace(/\.md$/, ''))
+
+  return {
+    paths: fileSlugs.map(fileSlug => ({ params: { slug: fileSlug } })),
+    fallback: false
+  }
 }
 
 export default PostDetailPage
