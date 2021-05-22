32ef5a38a7b8ca8b1e7d680df69ba56f98b456f6
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-05-22 14:43:36 +0900

    Add post header & content components

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,19 @@
+import PostHeader from './post-header'
+import classes from './post-content.module.css'
+import { DUMMY_POSTS } from '../../../pages/index'
+
+const DUMMY_POST = DUMMY_POSTS[0]
+DUMMY_POST.content = '# This is a first post'
+
+function PostContent(props) {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  )
+}
+
+export default PostContent
