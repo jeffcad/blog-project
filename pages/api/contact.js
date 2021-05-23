2091769bb870613b511f111118976e32827eed4f
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-05-23 10:46:32 +0900

    Connect contact API route to MongoDB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,7 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb'
+import { databasePassword } from '../../credentials'
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body
 
@@ -19,8 +22,26 @@ function handler(req, res) {
       name,
       message
     }
-    console.log(newMessage)
-    res.status(201).json({ message: 'Message successfully stored' })
+
+    let client
+
+    try {
+      client = await MongoClient.connect(`mongodb+srv://admin:${databasePassword}@cluster0.wsw5b.mongodb.net/blog?retryWrites=true&w=majority`)
+    } catch (error) {
+      res.status(500).json({ message: 'Unable to connect to database.' })
+      return
+    }
+
+    try {
+      const db = client.db()
+      const result = await db.collection('messages').insertOne(newMessage)
+      newMessage._id = result.insertedId
+      res.status(201).json({ message: 'Message successfully stored' })
+    } catch (error) {
+      res.status(500).json({ message: 'Unable to add message to database.' })
+    }
+
+    client.close()
   }
 }
 
